6bbf14707ccc664031805fee30cc5d0c02662fc4
uladkonop <vladknpljov@gmail.com>
2021-07-18 13:27:42 +0300

    add account migration

---
+++ db/migrate/20210717124553_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :accounts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :currency, limit: 10
+      t.decimal :amount
+
+      t.timestamps
+    end
+  end
+end
