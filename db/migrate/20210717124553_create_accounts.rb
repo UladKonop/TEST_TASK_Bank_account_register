7b6dd9c53f269050c3fadaf6b33f78e52375a6da
uladkonop <vladknpljov@gmail.com>
2021-07-19 10:31:57 +0300

    add default value for amount in account

--- db/migrate/20210717124553_create_accounts.rb
+++ db/migrate/20210717124553_create_accounts.rb
@@ -3,7 +3,7 @@ class CreateAccounts < ActiveRecord::Migration[6.1]
     create_table :accounts do |t|
       t.references :user, null: false, foreign_key: true
       t.string :currency, limit: 10
-      t.decimal :amount
+      t.decimal :amount, default: 0
 
       t.timestamps
     end
