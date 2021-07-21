a36cdb26ef963f4afaa205e7fca6666eb07a290f
uladkonop <vladknpljov@gmail.com>
2021-07-21 13:15:02 +0300

    add routes for deposit transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :accounts, only: %i[show create destroy]
   resources :users
+  resources :transactions, only: %i[], param: :account_id do
+    post 'deposit', on: :member 
+  end
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
