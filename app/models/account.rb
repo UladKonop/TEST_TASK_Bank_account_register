8f78f1707dbadd6f9b584be2297035425fe58825
uladkonop <vladknpljov@gmail.com>
2021-07-21 13:17:18 +0300

    add deposit method to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,16 +2,19 @@
 
 class Account < ApplicationRecord
   belongs_to :user
-  
+
   before_validation :set_amount
 
   validates :currency, presence: true, uniqueness: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
+
+  def deposit(received_amount)
+    self.amount += received_amount
+  end
 
   private
 
   def set_amount
     self.amount ||= 0
   end
-
 end
