05d3e554ea55eff18012c4372add27ea81822d98
uladkonop <vladknpljov@gmail.com>
2021-07-23 14:57:00 +0300

    add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user do
+    described_class.new(first_name: 'first_name', last_name: 'last_name', patronimic: 'patronimic',
+                        identification_number: 1)
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without a firstname' do
+      user.first_name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a last name' do
+      user.last_name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a patronimic' do
+      user.patronimic = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without an identification number' do
+      user.identification_number = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
