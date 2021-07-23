2b1288606b8a0de7d93ce0a3dd2ac4f7fbc1de47
uladkonop <vladknpljov@gmail.com>
2021-07-23 15:31:48 +0300

    add transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,9 +2,13 @@
 
 class TransactionsController < ApplicationController
   def deposit
-    account = Account.find(params[:account_id])
-    account.deposit(params[:amount].to_i)
+    user = User.find_by(identification_number: params[:identification_number])
+    render_error({ "user": ['must exist'] }) and return if user.nil?
 
+    account = find_or_build_account(user)
+    render_error(account.errors) and return if account.invalid?
+
+    account.deposit(account_params[:amount].to_f)
     if account.save
       render json: account, status: :created, location: account
     else
@@ -12,9 +16,52 @@ class TransactionsController < ApplicationController
     end
   end
 
+  def transfer
+    sender = User.find_by(identification_number: params[:sender_identification_number])
+    render_error({ "sender": ['must exist'] }) and return if sender.nil?
+
+    recipient = User.find_by(identification_number: params[:recipient_identification_number])
+    render_error({ "recipient": ['must exist'] }) and return if recipient.nil?
+
+    sender_account = sender.accounts.find_by(currency: account_params[:currency])
+    render_error({ "sender": ['must exist'] }) and return if sender.nil?
+
+    Account.transaction do
+      amount = account_params[:amount].to_f
+      recipient_account = find_or_create_account(recipient)
+
+      [sender_account, recipient_account].sort_by(&:id).each(&:lock!)
+
+      sender_account.withdraw!(amount)
+      recipient_account.deposit!(amount)
+
+      head :ok
+    rescue ActiveRecord::RecordInvalid => e
+      render_error(error: e.message)
+    end
+  end
+
   private
 
-  def deposit_params
-    params.permit(:amount)
+  def find_or_create_account(user)
+    return nil unless user
+
+    account = user.accounts.find_by(currency: account_params[:currency])
+    account || Account.create(account_params.merge(user: user))
+  end
+
+  def find_or_build_account(user)
+    return nil unless user
+
+    account = user.accounts.find_by(currency: account_params[:currency])
+    account || Account.new(account_params.merge(user: user))
+  end
+
+  def account_params
+    params.require(:account).permit(:amount, :currency)
+  end
+
+  def render_error(error, status: :unprocessable_entity)
+    render(json: error, status: status) and return
   end
 end
