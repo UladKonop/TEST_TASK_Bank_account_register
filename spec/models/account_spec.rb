d4866c9d2a7249c36d1e0ae3b4af813e5a894a27
uladkonop <vladknpljov@gmail.com>
2021-07-24 15:20:55 +0300

    add rspec for account model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,45 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_attributes) do
+    {
+      first_name: 'first_name',
+      last_name: 'last_name',
+      patronimic: 'patronimic',
+      identification_number: 1
+    }
+  end
+
+  let(:account_attributes) do
+    {
+      currency: 'usd',
+      amount: '0.0'
+    }
+  end
+
+  let(:user) do
+    User.create! user_attributes
+  end
+
+  let(:account) do
+    user.accounts.create account_attributes
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(account).to be_valid
+    end
+
+    it 'is not valid without a currency' do
+      account.currency = nil
+      expect(account).not_to be_valid
+    end
+
+    it 'is not valid without a amount' do
+      account.amount = -1
+      expect(account).not_to be_valid
+    end
+  end
 end
