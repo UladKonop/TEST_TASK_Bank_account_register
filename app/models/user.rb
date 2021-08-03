aa90b6833e89080ffd4f6984988d17a818328afc
uladkonop <vladknpljov@gmail.com>
2021-08-03 11:08:21 +0300

    add tags validation, accepting nested attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@
 class User < ApplicationRecord
   has_many :tags, dependent: :destroy
   has_many :accounts, dependent: :destroy
+
+  accepts_nested_attributes_for :tags
   
-  validates :first_name, :last_name, :patronimic, :identification_number, presence: true
+  validates :first_name, :last_name, :patronimic, :identification_number, :tags, presence: true
   validates :identification_number, uniqueness: true
 end
