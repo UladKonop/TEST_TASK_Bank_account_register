e0a70cf4dd90e10726a59dbf24899b6b3807f8d8
uladkonop <vladknpljov@gmail.com>
2021-08-05 23:37:55 +0300

    add deposits report

--- app/services/reports_manager.rb
+++ app/services/reports_manager.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class ReportsManager
+  DEPOSIT_REPORT_COLUMNS = %i[currency amount user]
+
   attr_reader :params
+
+  def initialize(params)
+    @params = params
+  end
 
   def call
     case params[:report_type]
@@ -18,31 +24,32 @@ class ReportsManager
 
   private
 
-  def initialize(params)
-    @params = params
-  end
-
   def deposits_report
-    scoped_transactions.where(deposit: true).pluck('accounts.currency',
-                                                   'amount').group_by(&:first)
+    scoped_transactions
+      .where(deposit: true)
+      .pluck('accounts.currency', 'amount', 'users.last_name')
+      .map { |row| DEPOSIT_REPORT_COLUMNS.zip(row).to_h }
+      .group_by { |row| row.delete(:currency) }
   end
 
-  def measures_report; end
-
+  def measures_report;  end
+  
   def total_amount_report; end
-
+  
   def scoped_transactions
     @scoped_transactions ||=
-      Transaction
-      .joins(:account)
-      .then { |transactions| params[:user_ids].present? ? transactions.where('accounts.user_id' => params[:user_ids]) : transactions }
-      .then do |transactions|
+    Transaction
+    .joins(:account)
+    .joins(account: :user)
+    .joins(account: [user: :tags])
+    .then { |transactions| params[:user_ids].present? ? transactions.where('accounts.user_id' => params[:user_ids]) : transactions }
+      .then { |transactions|
         if params[:date_from].present?
           transactions.where('transactions.created_at > ?', params[:date_from].to_date)
         else
           transactions
         end
-      end
+      }
       .then do |transactions|
         if params[:date_to].present?
           transactions.where('transactions.created_at < ?', params[:date_to].to_date)
@@ -50,5 +57,6 @@ class ReportsManager
           transactions
         end
       end
+      .then { |transactions| params[:tags].present? ? transactions.where('tags.name' => params[:tags]) : transactions }
   end
 end
